caedc25edcb5666e5db7b0d63b99d0a2e76f9d9b
yiningread <108318704+yiningread@users.noreply.github.com>
2022-10-01 08:26:24 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,127 +1,95 @@
-<?php
-error_reporting(0);// 这行是屏蔽所有可能出现的错误信息
-header("Content-Type: text/html; charset=utf-8");
-
-define('a', '本程序由火端网络开发，官方网站：http://www.huoduan.com，源码唯一销售客服QQ号码：909516866 ，请尊重开发者劳动成果，勿将本程序发布到网上或倒卖，感谢您的支持！');//此信息不影响程序正常使用，请勿修改此处信息，修改后程序肯定会出错，因修改此信息引起的错误，将不提供任何技术支持
-
-define('ROOT_PATH',dirname(__FILE__));
-
-/* 
- * @Author: http://www.huoduan.com
- * @ Email: admin@huoduan.com
- * @    QQ: 909516866
- */
-
-include("data/huoduan.config.php");
-define('REWRITE',$huoduan['rewrite']);
-define('SYSPATH',$huoduan['path']);
-define('PCDOMAIN',$huoduan['pcdomain']);
-define('MOBILEDOMAIN',$huoduan['mobiledomain']);
-define('URLRULE1',$huoduan['urlrule1']);
-define('URLRULE2',$huoduan['urlrule2']);
-$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
-define('HTTP',$sys_protocal);
-define('c',ROOT_PATH.'/inc/core.php');
-include("inc/function.php");
-include('inc/core.php');
-
-$host = $_SERVER['HTTP_HOST'];
-$ip = get_ip();
-
-if(strpos('|'.$huoduan['ip'].'|','|'.$ip.'|')>-1){
-   echo '禁止访问';
-   exit;	
+<?php 
+include('config.php');
+include('admincore.php');
+if(isset($_GET['phpinfo'])){
+	phpinfo();exit;
 }
-if(isset($_GET['rewrite'])){
-	$rewrite = $_GET['rewrite'].sha1(a);
-	$rewrite = ltrim($rewrite,'/');
-	
-	
-	if(strpos(URLRULE1,'{qe}')>-1){
-		$rulekey1 = '{qe}';
-	}else{
-		$rulekey1 = '{q}';
-	}
-    $rule1 = str_replace('.','\\.',URLRULE1);
-	$rule1 = str_replace('/','\/',$rule1);
-	$rule1 = str_replace($rulekey1,'(.+?)',$rule1);
-	$rule1 = "/".$rule1.trim(sha1(a),'1')."1/";
-	$rule1 = str_replace(' ','',$rule1);
-	
-	if(strpos(URLRULE2,'{qe}')>-1){
-		$rulekey2 = '{qe}';
-	}else{
-		$rulekey2 = '{q}';
-	}
-
-	$rule2 = str_replace('.','\\.',URLRULE2);
-	$rule2 = str_replace('/','\/',$rule2);
-	$rule2 = str_replace($rulekey2,'(.+?)',$rule2);
-	$rule2 = str_replace('{p}','([0-9]\d*)',$rule2);
-	$rule2 = "/".$rule2."d".trim(sha1(a),'d')."/";
-	$rule2 = str_replace(' ','',$rule2);
-	
-	if(preg_match("/sitemap\/(.+?)_([0-9]\d*).html/", $rewrite, $match)){
-		$_GET['name']=$match[1];
-		$_GET['p']=$match[2];
-		include('inc/sitemap.php');
-	    exit;
-	}else if(preg_match("/sitemap\/(.+?).html/", $rewrite, $match)){
-		$_GET['name']=$match[1];
-		include('inc/sitemap.php');
-	    exit;
-	}else if($rewrite=='sitemap/'.sha1(a)){
-		include('inc/sitemap.php');
-	    exit;
-	}else if(preg_match($rule2, $rewrite, $match)){
-		
-		$_GET['q']=$match[1];
-		$_GET['p']=$match[2];
-	}else if(preg_match($rule1, $rewrite, $match)){
-		$_GET['q']=$match[1];
-	}else{
-		header('HTTP/1.1 404 Not Found');
-        header("status: 404 Not Found");
-		if(is_file('404.html')){
-			echo file_get_contents('404.html');
-		}else{
-        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>对不起！你访问的页面不存在 - 404</title></head><body>对不起！你访问的页面不存在，<a href="'.SYSPATH.'#v404">返回首页</a></body></html>';
-		}
-exit;
-	}
-    
-	
-	include('inc/search.php');
-	exit;
-}
-if(isset($_GET['q'])){
-	include('inc/search.php');
-	exit;
-}
-if(isset($_GET['a'])){
-	$a = $_GET['a'];
-	switch($a){
-		case 'url':
-		  include('inc/url.php');
-	      exit;
-		break;
-		case 'code':
-		  include('inc/code.php');
-	      exit;
-		break;
-		case 'go':
-		  include('data/huoduan.baidutop.php');
-		  shuffle($topkey);
-		  $url = huoduansourl($topkey[0]);
-		  header("location: $url");
-		break;
-		case 'sitemap':
-		  include('inc/sitemap.php');
-	      exit;
-		break;
-	}
-	
-}
-include('inc/home.php');
-if(isset($_GET['hd'])){echo a(a);}
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php include('inc.php'); ?>
+</head>
+<body>
+<?php $nav = 'home';include('head.php'); ?>
+
+<div id="hd_main">
+
+   <div style="margin:20px;">
+   <?php
+    $path = $_SERVER['SCRIPT_NAME'];
+    if(strpos($path,'/admin/')>-1){
+	   echo '<div style="text-align:center; color:red; font-size:16px; margin-bottom:15px;">您的后台目录为默认的/admin/ ，为了安全请修改后台目录</div>';	
+	}
+	if($huoduan['admin_name']=='huoduan' && $huoduan['admin_pass']=='a13a02276910cbc879f020ed88816512'){
+	   echo '<div style="text-align:center; color:red; font-size:16px; margin-bottom:15px;">您的账号密码为系统默认，请尽快修改</div>';	
+	}
+   ?>
+
+
+      <table width="600" border="0" class="tablecss" cellspacing="0" cellpadding="0" align="center">
+   <tr>
+    <td colspan="2" align="center">欢迎使用火端搜索系统！</td>
+    </tr>
+  <tr>
+    <td align="right">当前搜索程序版本：</td>
+    <td><span id="m_ver"><a href="http://www.huoduan.com/search-code.html" target="_blank">V2.1</a></span></td>
+  </tr>
+  <tr>
+    <td align="right">最新版本：</td>
+    <td><span id="new_ver"><a href="http://www.huoduan.com/search-code.html" target="_blank">V2.1</a></span></td>
+  </tr>
+  <tr>
+    <td width="213" align="right">服务器操作系统：</td>
+    <td width="387"><?php $os = explode(" ", php_uname()); echo $os[0];?> (<?php if('/'==DIRECTORY_SEPARATOR){echo $os[2];}else{echo $os[1];} ?>)</td>
+  </tr>
+  <tr>
+    <td width="213" align="right">服务器解译引擎：</td>
+    <td width="387"><?php echo $_SERVER['SERVER_SOFTWARE'];?></td>
+  </tr>
+  <tr>
+    <td width="213" align="right">PHP版本：</td>
+    <td width="387"><a href="./index.php?phpinfo=1" target="_blank"><?php echo PHP_VERSION?></a></td>
+  </tr>
+  <tr>
+    <td align="right">域名：</td>
+    <td><?php echo $_SERVER['HTTP_HOST']?></td>
+  </tr>
+  <tr>
+    <td align="right">allow_url_fopen：</td>
+    <td><?php echo ini_get('allow_url_fopen') ? '<span class="green">支持</span>' : '<span class="red">不支持</span>'?></td>
+  </tr>
+  <tr>
+    <td align="right">curl_init：</td>
+    <td><?php if ( function_exists('curl_init') ){ echo '<span class="green">支持</span>' ;}else{ echo '<span class="red">不支持</span>';}?></td>
+  </tr>
+<tr>
+    <td align="right">/cache/目录权限检测：</td>
+    <td><?php echo is_writable('../cache/') ? '<span class="green">可写</span>' : '<span class="red">不可写</span>'?></td>
+  </tr>
+<tr>
+    <td align="right">/data/目录权限检测：</td>
+    <td><?php echo is_writable('../data/') ? '<span class="green">可写</span>' : '<span class="red">不可写</span>'?></td>
+  </tr>  
+  <tr>
+    <td align="right">搜索主页：</td>
+    <td><a href="../" target="_blank">访问主页</a></td>
+  </tr>
+  
+  <tr>
+    <td colspan="2" style="line-height:220%; text-indent:28px;">本程序由<a href="http://www.huoduan.com" target="_blank">火端网络</a>开发，官方网站：<a href="http://www.huoduan.com" target="_blank">http://www.huoduan.com</a>，源码唯一销售客服QQ号码：<a href="http://wpa.qq.com/msgrd?v=3&uin=909516866&site=qq&menu=yes" target="_blank">909516866</a> ，请尊重开发者劳动成果，勿将本程序发布到网上或倒卖，感谢您的支持！</td>
+    </tr>
+   
+</table>
+
+   </div>
+
+</div><!--main-->
+
+
+<?php include('foot.php'); ?>
+<!-- 检测新版本 -->
+<script type="text/javascript" src="http://www.huoduan.com/app/web/so.php?c=update&ver=2.1&z=21new"></script>
+</body>
+</html>
